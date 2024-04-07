73fb5071d8abd7385173776825805204601b11cb
Sushant Bherwal <sushantbharwal@gmail.com>
2024-04-07 13:34:32 +0530

    Update ArticleCard.tsx

--- src/components/ArticlesSection/ArticleCard.tsx
+++ src/components/ArticlesSection/ArticleCard.tsx
@@ -17,7 +17,7 @@ export default function ArticleCard({ article }: ArticleCardProps) {
         bgGradient="linear(to-r, #1f2544, #095379)"
         p={4}
         boxShadow="xl"
-        height={{ base: "40vh", lg: "30vh" }}
+        height={{ base: "50vh", lg: "30vh" }}
         textAlign="left"
       >
         <Avatar
